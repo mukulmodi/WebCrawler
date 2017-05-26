56928f74298444ba3139a6e1ea04e84a1db8d1ec
Mukul Modi <mukul.modi@gmail.com>
2017-05-25 23:46:08 -0500

    Update SpiderTest.java

--- SpiderTest.java
+++ SpiderTest.java
@@ -7,6 +7,6 @@ public class SpiderTest
 	public static void main(String[] args)
 	{
 		Spider spider = new Spider();
-		spider.search("http://www.yahoo.com/", "india");
+		spider.search("http://www.cnn.com/", "india");
 	}
 }
